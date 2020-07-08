cbfae10b225714705c5638a83af8a759b464c97c
Charlles Abreu <abreu@eq.ufrj.br>
2020-07-08 11:24:13 -0300

    Updated Manifest.toml

--- test/runtests.jl
+++ test/runtests.jl
@@ -19,25 +19,20 @@ import Chemfiles
 
 function test_compute_nonbonded(xyz_file, L, cutoff, switch)
     xyz_data = [row[i] for i=2:4, row in eachrow(readdlm(xyz_file; skipstart=2))]
-
     positions = CUDA.cu(xyz_data)
     N = size(xyz_data, 2)
-
     model = LennardJonesModel(cutoff, switch)
     atoms = CUDA.cu(fill(LennardJonesAtom(1, 1), N))
 
     forces_ref = CUDA.zeros(Float32, 3, N)
     energies_ref = CUDA.zeros(N)
     virials_ref = CUDA.zeros(N)
-
     naively_compute_nonbonded!(forces_ref, energies_ref, virials_ref, positions, L, model, atoms)
 
     tiles = nonbonded_computation_tiles(N)
-
     forces = CUDA.zeros(Float32, 3, N)
     energies = CUDA.zeros(N)
     virials = CUDA.zeros(N)
-
     compute_nonbonded!(forces, energies, virials, positions, L,
                        tiles, model, atoms, Val(FORCES | ENERGIES | VIRIALS))
 
