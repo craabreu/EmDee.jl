707c7baf53c632e8d8a2400599dea0f0212fe318
Charlles Abreu <abreu@eq.ufrj.br>
2020-07-12 12:46:57 -0300

    PDB Aliases with elements and sanitazed string literals

--- devtools/update_pdb_aliases.jl
+++ devtools/update_pdb_aliases.jl
@@ -24,11 +24,10 @@ function pdb_standard_bonds(residues, pdb_names)
 
     residues_root = root(parse_file(residues))
     residue_dict = OrderedDict{String, Vector{Vector{String}}}()
-    regex(x) = "\\b($(replace(x, "-" => "_")))\\b"
     for element in residues_root["Residue"]
         attributes = attributes_dict(element)
         name = attributes["name"]
-        residue_dict[name] = [map(x->get(glossary[name], x, regex(x)),
+        residue_dict[name] = [map(x->get(glossary[name], x, "\\b($x)\\b"),
                               values(attributes_dict(item)))
                               for item in element["Bond"]]
         for alt in alternatives[name]
@@ -38,16 +37,35 @@ function pdb_standard_bonds(residues, pdb_names)
     return residue_dict
 end
 
+const ELEMENTS = LittleDict(
+    "H" => 1.008,
+    "C" => 12.011,
+    "N" => 14.007,
+    "O" => 15.999,
+    "P" => 30.973762,
+    "S" => 32.06,
+)
+
+sanitized(str) = replace(replace(replace(str, "-" => "_"), "'" => "p"), "*" => "a")
+
 function pdb_aliases_xml(pdb_bonds)
     regex_set = OrderedSet(regex for list in values(pdb_bonds) for bond in list for regex in bond)
 
     xdoc = XMLDocument()
     xroot = create_root(xdoc, "Residues")
+    elements = new_child(xroot, "Elements")
+    for (element, mass) in ELEMENTS
+        set_attributes(
+            new_child(elements, "Element"),
+            Dict("name" => element, "mass" => mass),
+        )
+    end
+
     regular_expressions = new_child(xroot, "RegularExpressions")
     regex_id = Dict()
     for (id, regex) in enumerate(regex_set)
         regex_entry = new_child(regular_expressions, "Regex")
-        set_attributes(regex_entry, Dict("id" => id, "code" => regex))
+        set_attributes(regex_entry, Dict("id" => id, "code" => sanitized(regex)))
         regex_id[regex] = id
     end
 
@@ -70,8 +88,8 @@ for file in files
 end
 
 pdb_bonds = pdb_standard_bonds(broadcast(joinpath, @__DIR__, pipes)...)
-pdb_aliases = pdb_aliases_xml(pdb_bonds)
+pdb_aliases_content = pdb_aliases_xml(pdb_bonds)
 
 io = open(joinpath(@__DIR__, "..", "src", "data", "pdb_aliases.xml"), "w")
-write(io, string(pdb_aliases))
+write(io, string(pdb_aliases_content))
 close(io)
