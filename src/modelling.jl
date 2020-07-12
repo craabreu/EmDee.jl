181fc210bcec26ea2eaf58de3cb68480874eb557
Charlles Abreu <abreu@eq.ufrj.br>
2020-07-11 23:57:25 -0300

    Assignment of force-field types and charges to system atoms

--- src/modelling.jl
+++ src/modelling.jl
@@ -1,5 +1,4 @@
-export ForceField,
-       System
+export ForceField, System
 
 import LightXML
 import Chemfiles
@@ -18,7 +17,7 @@ struct ForceField
     dihedral_types::DataFrame
     improper_types::DataFrame
     nonbonded::DataFrame
-    templates::Vector{ResidueTemplate}
+    templates::OrderedDict{String,ResidueTemplate}
     lj₁₋₄::Float64
     coulomb₁₋₄::Float64
 end
@@ -29,37 +28,71 @@ else
     function Chemfiles_atoms(residue::Chemfiles.Residue)
         count = size(residue)
         result = Array{UInt64}(undef, count)
-        Chemfiles.__check(Chemfiles.lib.chfl_residue_atoms(Chemfiles.__const_ptr(residue), pointer(result), count))
+        Chemfiles.__check(Chemfiles.lib.chfl_residue_atoms(
+            Chemfiles.__const_ptr(residue),
+            pointer(result),
+            count,
+        ))
         return result
     end
 end
 
-const ATOM_TYPE = LittleDict(:name=>String, :class=>String, :element=>String, :mass=>Float64)
+const ATOM_TYPE =
+    LittleDict(:name => String, :class => String, :element => String, :mass => Float64)
 
-const HARMONIC_BOND = LittleDict(:type1=>String, :type2=>String,
-                                 :class1=>String, :class2=>String,
-                                 :length=>Float64, :k=>Float64)
+const HARMONIC_BOND = LittleDict(
+    :type1 => String,
+    :type2 => String,
+    :class1 => String,
+    :class2 => String,
+    :length => Float64,
+    :k => Float64,
+)
 
-const HARMONIC_ANGLE = LittleDict(:type1=>String, :type2=>String, :type3=>String,
-                                  :class1=>String, :class2=>String, :class3=>String,
-                                  :angle=>Float64, :k=>Float64)
+const HARMONIC_ANGLE = LittleDict(
+    :type1 => String,
+    :type2 => String,
+    :type3 => String,
+    :class1 => String,
+    :class2 => String,
+    :class3 => String,
+    :angle => Float64,
+    :k => Float64,
+)
 
-const PERIODIC_TORSION = LittleDict(:type1=>String, :type2=>String, :type3=>String, :type4=>String,
-                                    :class1=>String, :class2=>String, :class3=>String, :class4=>String,
-                                    :periodicity1=>Int, :phase1=>Float64, :k1=>Float64,
-                                    :periodicity2=>Int, :phase2=>Float64, :k2=>Float64,
-                                    :periodicity3=>Int, :phase3=>Float64, :k3=>Float64,
-                                    :periodicity4=>Int, :phase4=>Float64, :k4=>Float64)
+const PERIODIC_TORSION = LittleDict(
+    :type1 => String,
+    :type2 => String,
+    :type3 => String,
+    :type4 => String,
+    :class1 => String,
+    :class2 => String,
+    :class3 => String,
+    :class4 => String,
+    :periodicity1 => Int,
+    :phase1 => Float64,
+    :k1 => Float64,
+    :periodicity2 => Int,
+    :phase2 => Float64,
+    :k2 => Float64,
+    :periodicity3 => Int,
+    :phase3 => Float64,
+    :k3 => Float64,
+    :periodicity4 => Int,
+    :phase4 => Float64,
+    :k4 => Float64,
+)
 
-const NONBONDED = LittleDict(:type=>String, :charge=>Float64, :sigma=>Float64, :epsilon=>Float64)
+const NONBONDED =
+    LittleDict(:type => String, :charge => Float64, :sigma => Float64, :epsilon => Float64)
 
-Base.convert(::Type{T}, x::S) where {T<:Number, S<:AbstractString} = parse(T, x)
+Base.convert(::Type{T}, x::S) where {T<:Number,S<:AbstractString} = parse(T, x)
 
 Base.zero(::Type{String}) = ""
 
 function DataFrame(category, element_list, key)
-    df = DataFrame((collect∘values)(category), (collect∘keys)(category))
-    zeros = LittleDict(string(key)=>zero(value) for (key, value) in category)
+    df = DataFrame((collect ∘ values)(category), (collect ∘ keys)(category))
+    zeros = LittleDict(string(key) => zero(value) for (key, value) in category)
     attribute_dicts(list, key) = [LightXML.attributes_dict(a) for e in list for a in e[key]]
     for dict in attribute_dicts(element_list, key)
         append!(df, merge(zeros, dict))
@@ -71,7 +104,7 @@ function ForceField(xml_file)
     xroot = LightXML.root(LightXML.parse_file(xml_file))
     atom_types = DataFrame(ATOM_TYPE, xroot["AtomTypes"], "Type")
     type_index = LittleDict(atom_types.name .=> collect(1:nrow(atom_types)))
-    templates = Vector{ResidueTemplate}()
+    templates = OrderedDict{String,ResidueTemplate}()
     for elem in xroot["Residues"]
         for (residue_index, residue_item) in enumerate(elem["Residue"])
             atoms = []
@@ -88,24 +121,27 @@ function ForceField(xml_file)
                 push!(atoms, atom)
                 atom_index[name] = index
             end
-            natoms = length(atoms) + length(residue_item["ExternalBond"])
+            natoms = length(atoms) #+ length(residue_item["ExternalBond"])
             adjmat = falses(natoms, natoms)
             for bond in residue_item["Bond"]
-                i, j = [key ∈ ["to", "from"] ? parse(Int, value) + 1 : atom_index[value]
-                        for (key, value) in LightXML.attributes_dict(bond)]
+                i, j = [
+                    key ∈ ["to", "from"] ? parse(Int, value) + 1 : atom_index[value]
+                    for (key, value) in LightXML.attributes_dict(bond)
+                ]
                 adjmat[i, j] = adjmat[j, i] = true
             end
-            for bond in residue_item["ExternalBond"]
-                for (key, value) in LightXML.attributes_dict(bond)
-                    name = key == "from" ? Chemfiles.name(atoms[parse(Int, value) + 1]) : value
-                    push!(atoms, Chemfiles.Atom("$(name)*"))
-                    i = atom_index[name]
-                    j = length(atoms)
-                    adjmat[i, j] = adjmat[j, i] = true
-                end
-            end
-            order, adjmat = canonical_form(adjmat)
-            push!(templates, ResidueTemplate(atoms[order], adjmat))
+            # for bond in residue_item["ExternalBond"]
+            #     for (key, value) in LightXML.attributes_dict(bond)
+            #         name = key == "from" ? Chemfiles.name(atoms[parse(Int, value) + 1]) : value
+            #         push!(atoms, Chemfiles.Atom("$(name)*"))
+            #         i = atom_index[name]
+            #         j = length(atoms)
+            #         adjmat[i, j] = adjmat[j, i] = true
+            #     end
+            # end
+            order, adjmat = canonical_form(adjmat, Chemfiles.mass.(atoms))
+            residue_name = LightXML.attribute(residue_item, "name")
+            templates[residue_name] = ResidueTemplate(atoms[order], adjmat)
         end
     end
 
@@ -119,31 +155,17 @@ function ForceField(xml_file)
     lj₁₋₄ = get(scaling_factors, "lj14scale", 1.0)
     coulomb₁₋₄ = get(scaling_factors, "coulomb14scale", 1.0)
 
-    return ForceField(atom_types, bonds, angles, dihedrals, impropers, nonbonded,
-                      templates, lj₁₋₄, coulomb₁₋₄)
-end
-
-function apply_force_field!(atom_list, adjacency, force_field)
-    num_residue_matches = 0
-    for (residue_index, matrix) in enumerate(force_field.adjacency)
-        if matrix == adjacency
-            topology = Chemfiles.Topology(force_field.frame)
-            residue = Chemfiles.Residue(topology, residue_index-1)
-            residue_atoms = Chemfiles_atoms(residue)
-            num_atom_matches = 0
-            for (index, atom_index) in enumerate(residue_atoms)
-                atom = atom_list[index]
-                template = Chemfiles.Atom(topology, atom_index)
-                if isapprox(Chemfiles.mass(atom), Chemfiles.mass(template), atol=0.1)
-                    num_atom_matches += 1
-                end
-                Chemfiles.set_property!(atom, "ff.type", Chemfiles.type(template))
-                Chemfiles.set_property!(atom, "ff.charge", Chemfiles.charge(template))
-            end
-            num_atom_matches == length(residue_atoms) && (num_residue_matches += 1)
-        end
-    end
-    return num_residue_matches
+    return ForceField(
+        atom_types,
+        bonds,
+        angles,
+        dihedrals,
+        impropers,
+        nonbonded,
+        templates,
+        lj₁₋₄,
+        coulomb₁₋₄,
+    )
 end
 
 function pdb_aliases(pdb_aliases_file)
@@ -173,20 +195,23 @@ function System(file, force_field)
     num_residues = Chemfiles.count_residues(topology)
     atoms = [Chemfiles.Atom(topology, index) for index = 0:num_atoms-1]
     residues = [Chemfiles.Residue(topology, index) for index = 0:num_residues-1]
-    residue_atoms = map((x->x.+1) ∘ Chemfiles_atoms, residues)
-    atom_residues = Vector{Int}(undef, num_atoms)
+    residue_atoms = map((x -> x .+ 1) ∘ Chemfiles_atoms, residues)
+    atom_residue = Vector{Int}(undef, num_atoms)
     internal_map = Vector{Int}(undef, num_atoms)
     for (index, atoms_list) in enumerate(residue_atoms)
-        atom_residues[atoms_list] .= index
+        atom_residue[atoms_list] .= index
         internal_map[atoms_list] .= collect(1:length(atoms_list))
     end
+
     is_std_pdb = Chemfiles.property.(residues, "is_standard_pdb")
-    bonds = [collect(bond)
-             for bond in eachcol(Chemfiles.bonds(topology) .+ 1)
-             if !all(is_std_pdb[atom_residues[bond]])]
+    bonds = [
+        collect(bond)
+        for
+        bond in eachcol(Chemfiles.bonds(topology) .+ 1) if !all(is_std_pdb[atom_residue[bond]])
+    ]
     chain_id = ""
     previous_indices = previous_names = []
-    for index in 1:num_residues
+    for index = 1:num_residues
         residue = residues[index]
         atom_indices = residue_atoms[index]
         atom_names = Chemfiles.name.(atoms[atom_indices])
@@ -205,37 +230,53 @@ function System(file, force_field)
                 from === nothing || to === nothing || push!(bonds, combined_indices[[from, to]])
             end
             previous_indices = atom_indices
-            previous_names = "_".*atom_names
+            previous_names = "_" .* atom_names
         end
     end
 
-    # adjacency, mapping = canonical_mapping!(residue_atoms, bonds)
+    adjacency_matrices = map(n -> falses(n, n), length.(residue_atoms))
+    for (atom_1, atom_2) in bonds
+        index = atom_residue[atom_1]
+        if atom_residue[atom_2] == index
+            i = internal_map[atom_1]
+            j = internal_map[atom_2]
+            adjacency_matrices[index][i, j] = adjacency_matrices[index][j, i] = true
+        end
+    end
 
-    return bonds
-    
+    for (residue, indices, matrix) in zip(residues, residue_atoms, adjacency_matrices)
+        atom_masses = Chemfiles.mass.(atoms[indices])
+        canonical_order, canonical_matrix = canonical_form(matrix, atom_masses)
+        matches = [n for (n, t) in force_field.templates if t.adjacency == canonical_matrix]
+        name = Chemfiles.name(residue)
+        length(matches) == 0 &&
+            error("No force field templates matched residue $(name)")
+        length(matches) > 1 &&
+            error("Multiple force field templates $(matches) matched residue $(name)")
+        template_atoms = force_field.templates[matches[1]].atoms
+        for (i, a) in zip(indices[canonical_order], template_atoms)
+            Chemfiles.set_property!(atoms[i], "ff.type", Chemfiles.type(a))
+            Chemfiles.set_property!(atoms[i], "ff.charge", Chemfiles.charge(a))
+        end
+    end
+
     system = Chemfiles.Frame()
     Chemfiles.set_cell!(system, unit_cell)
-    # has_velocities && Chemfiles.add_velocities!(system)
-    # for (index, residue) in enumerate(residues)
-    #     name = Chemfiles.name(residue)
-    #     matches = apply_force_field!(atoms[residue_atoms[index]], adjacency[index], force_field)
-    #     if matches != 1
-    #         println("$(matches == 0 ? "No" : "Multiple") force field templates for residue $(name)")
-    #     end
-    #     new_residue = Chemfiles.Residue(name, index)
-    #     for i in residue_atoms[index]
-    #         Chemfiles.add_atom!(system, atoms[i], positions[:, i], velocities[:, i])
-    #         Chemfiles.add_atom!(new_residue, mapping[i])
-    #     end
-    #     for property in Chemfiles.list_properties(residue)
-    #         Chemfiles.set_property!(new_residue, property, Chemfiles.property(residue, property))
-    #         @show property Chemfiles.property(residue, property)
-    #     end
-    #     Chemfiles.add_residue!(system, new_residue)
-    # end
-    # bond_orders = Chemfiles.bond_orders(topology)
-    # for ((atom_1, atom_2), order) in zip(eachcol(bonds), bond_orders)
-    #     Chemfiles.add_bond!(system, mapping[atom_1], mapping[atom_2], order)
-    # end
+    has_velocities && Chemfiles.add_velocities!(system)
+    position = Vector{Int}(undef, num_atoms)
+    atom_index = 0
+    for (index, residue) in enumerate(residues)
+        new_residue = Chemfiles.Residue(Chemfiles.name(residue), index)
+        for i in residue_atoms[index]
+            Chemfiles.add_atom!(system, atoms[i], positions[:, i], velocities[:, i])
+            Chemfiles.add_atom!(new_residue, atom_index)
+            position[i] = atom_index
+            atom_index += 1
+        end
+        Chemfiles.add_residue!(system, new_residue)
+    end
+    for bond in bonds
+        Chemfiles.add_bond!(system, position[bond]...)
+    end
     return system
 end
